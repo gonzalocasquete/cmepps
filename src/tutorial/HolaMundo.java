ea4fd3abf064cdafc02190d243e761c8f0274f51
usuario <usuario@pc21-082>
2022-10-04 16:59:45 +0200

    Saludo al usuario

--- src/tutorial/HolaMundo.java
+++ src/tutorial/HolaMundo.java
@@ -5,6 +5,9 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		
+		String name="Gonzalo";
+		System.out.println("Bienvenido, "+name);
 	}
 
 }
